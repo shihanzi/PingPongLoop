adcdf2cbae23e0f0c98e22943b9192ed496f2d8e
shihanze <shihanmails@gmail.com>
2024-11-04 08:15:03 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.Intrinsics.X86;
+
+for (int i = 1; i < 101; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("PingPong");
+        continue;
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Ping");
+        continue;
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Pong");
+        continue;
+    }
+    Console.WriteLine(i);
+}
+    
